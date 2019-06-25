41a71c464fe8f93cad4b436dbbc596ac54cf2bb8
pnowik <pawel.nowik.97@gmail.com>
2019-06-25 15:45:08 +0200

    Add tests

--- main.rb
+++ main.rb
@@ -1,37 +1,49 @@
 require 'nokogiri'
 require 'open-uri'
 require 'csv'
-require 'pry'
 
-def scraper
-
-  arr = Array.new
+class Scrape
+  arr = []
   doc = Nokogiri::HTML(open('https://www.otomoto.pl/osobowe/audi/a6/'))
-  doc.css("div.container-fluid.container-fluid-sm div.row ul.om-pager.rel").search("span.page").each do |page|
+  doc.css('div.container-fluid.container-fluid-sm div.row ul.om-pager.rel')
+      .search('span.page').each do |page|
     arr.push(page.text)
   end
-  pages = arr.last.to_i
+  puts number_of_pages = arr.last.to_i
 
-  CSV.open("file.csv", "wb") do |csv|
-    digits = 1..pages
-    csv << ["Rok produkcji", "Przebieg", "Pojemność silnika", "Rodzaj paliwa", "Cena"]
-    digits.each do |current_page|
+  CSV.open('file.csv', 'wb') do |csv|
+    pages = 1..3
+    pages.each do |current_page|
       doc = Nokogiri::HTML(open('https://www.otomoto.pl/osobowe/audi/a6/?page=' + current_page.to_s))
-      product_tile = doc.css("div.offer-item__content")
-
+      product_tile = doc.css('div.offer-item__content')
       product_tile.each do |product|
-        arr = Array.new
-        product.css("li.offer-item__params-item").search("span").each do |feature|
-          arr.push(feature.text.strip)
+        item_year = product.css('[data-code=year]')
+                             .text.gsub(/ +/, ' ').gsub(/\n/, '').strip
+        if item_year == ''
+          next
         end
-
-      item_price = product.css("div.offer-price span.offer-price__number").text.gsub(/ +/, " ").gsub(/\n/, "")
-      arr.push(item_price)
-      csv << [arr[0], arr[1], arr[2], arr[3], arr[4]]
+        item_mileage = product.css('[data-code=mileage]')
+                                .text.gsub(/ +/, ' ').gsub(/\n/, '').strip
+        if item_mileage == ''
+          next
+        end
+        item_engine_capacity = product.css('[data-code=engine_capacity]')
+                                        .text.gsub(/ +/, ' ').gsub(/\n/, '').strip
+        if item_engine_capacity == ''
+          next
+        end
+        item_fuel_type = product.css('[data-code=fuel_type]')
+                                  .text.gsub(/ +/, ' ').gsub(/\n/, '').strip
+        if item_fuel_type == ''
+          next
+        end
+        item_price = product.css('div.offer-price span.offer-price__number')
+                              .text.gsub(/ +/, ' ').gsub(/\n/, '').strip
+        if item_price == ''
+          next
+        end
+        csv << [item_year, item_mileage, item_engine_capacity, item_fuel_type, item_price]
       end
     end
   end
-
 end
-
-scraper
