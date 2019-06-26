c69b40810697cf4c12f34c0a962f422e99245211
pnowik <pawel.nowik.97@gmail.com>
2019-06-26 10:18:54 +0200

    Improve tests3

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 require 'csv'
 
-class Scrape
+def scrape
   arr = []
   doc = Nokogiri::HTML(open('https://www.otomoto.pl/osobowe/audi/a6/'))
   doc.css('div.container-fluid.container-fluid-sm div.row ul.om-pager.rel')
@@ -19,31 +19,18 @@ class Scrape
       product_tile.each do |product|
         item_year = product.css('[data-code=year]')
                              .text.gsub(/ +/, ' ').gsub(/\n/, '').strip
-       # if item_year == ''
-        #  next
-       # end
         item_mileage = product.css('[data-code=mileage]')
                                 .text.gsub(/ +/, ' ').gsub(/\n/, '').strip
-       # if item_mileage == ''
-        #  next
-       # end
         item_engine_capacity = product.css('[data-code=engine_capacity]')
                                         .text.gsub(/ +/, ' ').gsub(/\n/, '').strip
-       # if item_engine_capacity == ''
-        #  next
-       # end
         item_fuel_type = product.css('[data-code=fuel_type]')
                                   .text.gsub(/ +/, ' ').gsub(/\n/, '').strip
-       # if item_fuel_type == ''
-        #  next
-       # end
         item_price = product.css('div.offer-price span.offer-price__number')
                               .text.gsub(/ +/, ' ').gsub(/\n/, '').strip
-       # if item_price == ''
-         # next
-       # end
         csv << [item_year, item_mileage, item_engine_capacity, item_fuel_type, item_price]
       end
     end
   end
 end
+
+scrape
